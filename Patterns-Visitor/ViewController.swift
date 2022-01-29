8ce88c2ba14e970a7b736afe846e51e8d12627a3
Rus <de.mr44@gmail.com>
2022-01-29 11:41:25 +0500

    Implemented UITableViewDataSource protocol

--- Patterns-Visitor/ViewController.swift
+++ Patterns-Visitor/ViewController.swift
@@ -14,6 +14,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        registerCell()
+    }
+    
+    func registerCell() {
+        myTableView.register(FirstCell.self, forCellReuseIdentifier: "FirstCell")
+        myTableView.register(SecondCell.self, forCellReuseIdentifier: "SecondCell")
+        myTableView.register(ThirdCell.self, forCellReuseIdentifier: "ThirdCell")
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell = UITableViewCell()
+        
+        switch indexPath.row {
+        case 0:
+            cell = tableView.dequeueReusableCell(withIdentifier: "FirstCell", for: indexPath)
+            cell.backgroundColor = .systemGray
+        case 1:
+            cell = tableView.dequeueReusableCell(withIdentifier: "SecondCell", for: indexPath)
+            cell.backgroundColor = .systemMint
+        case 2:
+            cell = tableView.dequeueReusableCell(withIdentifier: "ThirdCell", for: indexPath)
+            cell.backgroundColor = .systemCyan
+        default:
+            break
+        }
+        
+        return cell
     }
 }
 
